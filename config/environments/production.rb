2364b21435610b64363e555e7c09f7b45c98a254
Matthew S. Drake <mattsdrake@gmail.com>
2013-04-01 23:13:01 -0400

    changed production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,9 +71,9 @@ Omrails::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['Onemonthrails'],
-      :access_key_id => ENV['AKIAIRBCKQEHN4THWLNA'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
